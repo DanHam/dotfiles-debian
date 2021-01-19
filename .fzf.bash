e216e19b661b13dbcf64738dee8c79a928e7b9f9
DanHam <DanHam@users.noreply.github.com>
2021-01-19 23:46:15 +0000

    Replace hard coded path with $HOME; Add fzf opts

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,17 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/dan/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/dan/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/dan/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/dan/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
+
+# Options
+# -------
+export FZF_DEFAULT_OPTS="--multi --bind 'ctrl-a:select-all'"
